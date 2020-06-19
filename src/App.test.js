98b54558c33e0325e19bb4a748537ad212a78bbd
adityawardhana <aditya.wardhana@elevenia.co.id>
2020-06-19 12:16:23 +0700

    add testing file

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import { render } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+import App from "./app"
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("full app rendering/navigating", () => {
+    const { container } = render(<App />)
+    expect(container.innerHTML).toMatch("progressbar")
+})
